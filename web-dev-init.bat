42511edaf41e97f57d4e28e8871586a5c8af0f4a
Ray <33094591+Ray6464@users.noreply.github.com>
2019-09-20 23:36:33 +0500

    another revision

--- web-dev-init.bat
+++ web-dev-init.bat
@@ -1,16 +1,16 @@
 mkdir myWeb && cd myWeb
 mkdir assets
 echo "document.body.innerHTML += 'Hello World';" >> index.js
-echo "<!DOCTYPE html>" >> index.html
-echo "<html>" >> index.html
-echo "<meta charset='UTF-8'>" >> index.html
-echo "<meta name='viewport' content='width=device-width, initial-scale=1'>" >> index.html
-echo "<link rel='stylesheet' href='main.css'>" >> index.html
-echo "<script src='index.js' defer></script>" >> index.html
-echo "<body>" >> index.html
-echo "<!-- our HTML code goes here -->" >> index.html
-echo "<h1>Welcome!</h1>" >> index.html
-echo "</body>" >> index.html
-echo "</html>" >> index.html
-echo "body { background-color:#fff; }" >> main.css
-echo "h1 { color:#131313; }" >> main.css
+echo <!DOCTYPE html> >> index.html
+echo <html> >> index.html
+echo <meta charset='UTF-8'> >> index.html
+echo <meta name='viewport' content='width=device-width, initial-scale=1'> >> index.html
+echo <link rel='stylesheet' href='main.css'> >> index.html
+echo <script src='index.js' defer></script> >> index.html
+echo <body> >> index.html
+echo <!-- our HTML code goes here --> >> index.html
+echo <h1>Welcome!</h1> >> index.html
+echo </body> >> index.html
+echo </html> >> index.html
+echo body { background-color:#fff; } >> main.css
+echo h1 { color:#131313; } >> main.css
