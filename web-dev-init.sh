fae671fb3009960185ab48f9e757be3441977a36
Ray <33094591+Ray6464@users.noreply.github.com>
2019-09-20 23:09:04 +0500

    Added scripts

---
+++ web-dev-init.sh
@@ -0,0 +1,24 @@
+mkdir myWeb && cd myWeb
+mkdir assets
+echo "<!DOCTYPE html>
+<html>
+<title>Our Website Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="main.css">
+<script src="index.js" defer></script>
+<body>
+
+<!-- our HTML code goes here -->
+<h1>Welcome!</h1>
+
+</body>
+</html>" >> index.html
+echo "body {
+  background-color:#fff;
+}
+h1 {
+  color:#131313;
+}
+" >> main.css
+echo "document.body.innerHTML += 'Hello World';" >> index.js
