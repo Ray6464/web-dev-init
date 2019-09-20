6655020b0cc749aacd491a9869391cdfe2f09132
Ray <33094591+Ray6464@users.noreply.github.com>
2019-09-20 23:26:11 +0500

    another revision

--- web-dev-init.bat
+++ web-dev-init.bat
@@ -1,5 +1,23 @@
 mkdir myWeb && cd myWeb
 mkdir assets
 echo "document.body.innerHTML += 'Hello World';" >> index.js
-echo "<!DOCTYPE html>^Z<html>^Z<title>Our Website Title</title>^Z<meta charset="UTF-8">^Z<meta name="viewport" content="width=device-width, initial-scale=1">^Z<link rel="stylesheet" href="main.css">^Z<script src="index.js" defer></script>^Z<body>^Z^Z<!-- our HTML code goes here -->^Z<h1>Welcome!</h1>^Z^Z</body>^Z</html>" >> index.html
-echo "body {^Z  background-color:#fff;^Z}^Zh1 {^Z  color:#131313;^Z}^Z" >> main.css
+echo "<!DOCTYPE html>^
+<html>^
+<title>Our Website Title</title>^
+<meta charset="UTF-8">^
+<meta name="viewport" content="width=device-width, initial-scale=1">^
+<link rel="stylesheet" href="main.css">^
+<script src="index.js" defer></script>^
+<body>^
+^
+<!-- our HTML code goes here -->^
+<h1>Welcome!</h1>^
+^
+</body>^
+</html>" >> index.html
+echo "body {^
+background-color:#fff;^
+}^
+h1 {^
+color:#131313;^
+}" >> main.css
