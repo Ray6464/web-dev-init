c09c4a167f66e8aa7e8e960faab3c98240fca60d
Ray <33094591+Ray6464@users.noreply.github.com>
2019-09-20 23:38:10 +0500

    another revision

--- web-dev-init.bat
+++ web-dev-init.bat
@@ -1,6 +1,7 @@
 mkdir myWeb && cd myWeb
 mkdir assets
 echo "document.body.innerHTML += 'Hello World';" >> index.js
+echo "" > index.html
 echo <!DOCTYPE html> >> index.html
 echo <html> >> index.html
 echo <meta charset='UTF-8'> >> index.html
@@ -12,5 +13,6 @@ echo <!-- our HTML code goes here --> >> index.html
 echo <h1>Welcome!</h1> >> index.html
 echo </body> >> index.html
 echo </html> >> index.html
+echo "" > main.css
 echo body { background-color:#fff; } >> main.css
 echo h1 { color:#131313; } >> main.css
